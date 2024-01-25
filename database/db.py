bc64b6a74256a8a857d4da6daba40d2c7dbd3ca1
Владислав <kant1755@mail.ru>
2024-01-25 18:59:48 +0300

    add + config database

---
+++ database/db.py
@@ -0,0 +1,39 @@
+from sqlalchemy import create_engine
+from typing import Generator
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
+from envparse import Env
+
+"""
+Block for common interaction with database
+"""
+env = Env()
+
+SQLALCHEMY_DATABASE_URL = env.str(
+    'SQLALCHEMY_DATABASE_URL',
+    default='postgresql+asyncpg://user:password@localhost/db'
+)
+
+# create async engine for interaction with database
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,
+    future=True,
+    echo=True,
+)
+
+# create session for the interaction with database
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+
+async def get_db() -> Generator:
+    """Dependency for getting async session"""
+    try:
+        session: AsyncSession = async_session()
+        yield session
+    finally:
+        await session.close()
+
+
+Base = declarative_base()
