cf9256a7de3784377312ba45a2f7756de9d37cbb
Владислав <kant1755@mail.ru>
2024-01-28 05:23:17 +0300

    add redoc_board.yaml, requirements.txt

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ app = FastAPI()
 app.include_router(user_routes, prefix="/user", tags=["user"])
 app.include_router(post_routes, prefix="/posts", tags=["posts"])
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
